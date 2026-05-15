5efcea28f97a8d76cc67f8b385e383c0783516a0
Dmitry <dmitri.cambur@gmail.com>
2026-05-15 15:04:59 +0300

    Initial QA automation project

---
+++ src/test/java/FirstSeleniumTest.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class FirstSeleniumTest {
+    public static void main(String[] args) {
+        git init
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        try {
+            driver.get("https://www.google.com");
+            String pageTitle = driver.getTitle();
+
+            System.out.println("Titlul paginii este: " + pageTitle);
+
+            if (pageTitle != null && pageTitle.contains("Google")) {
+                System.out.println("Test reușit: titlul paginii este corect.");
+            } else {
+                System.out.println("Test eșuat: titlul paginii nu este corect.");
+            }
+        } catch (Exception e) {
+            System.out.println("Eroare: " + e.getMessage());
+        } finally {
+            if (driver != null) {
+                driver.quit();
+            }
+        }
+    }
+}
